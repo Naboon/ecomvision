ae47eb45546ba229fccb13320b3251ae2028e064
Jesse Toivanen <jesse.toivanen2@gmail.com>
2023-02-09 14:17:39 +0200

    Add sidebar

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -4,12 +4,26 @@ import { Box, useMediaQuery } from '@mui/material'
 import { useSelector } from 'react-redux'
 
 import Navbar from 'components/Navbar'
+import Sidebar from 'components/Sidebar'
 
 const Layout = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true)
+
+  const isNonMobile = useMediaQuery('(min-width: 600px)')
+
   return (
-    <Box width='100%' height='100%'>
+    <Box display={isNonMobile ? 'flex' : 'block'} width='100%' height='100%'>
+      <Sidebar
+        isNonMobile={isNonMobile}
+        drawerWidth='250px'
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
       <Box>
-        <Navbar />
+        <Navbar
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
         <Outlet />
       </Box>
     </Box>

f2ac3f8fe0e875346b3a0cbaed20c0629215fef8
Jesse Toivanen <jesse.toivanen2@gmail.com>
2023-02-09 13:26:47 +0200

    Add navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import { useMemo } from 'react'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import { createTheme } from '@mui/material/styles'
 import { useSelector } from 'react-redux'
 
 import { themeSettings } from 'theme'
+import Dashboard from 'scenes/dashboard'
+import Layout from 'scenes/layout'
 
 function App() {
   const mode = useSelector((state) => state.global.mode)
@@ -11,9 +14,17 @@ function App() {
 
   return (
     <div className='app'>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path='/' element={<Navigate to='/dashboard' replace />} />
+              <Route path='/dashboard' element={<Dashboard />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ client/src/scenes/layout/index.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import { Outlet } from 'react-router-dom'
+import { Box, useMediaQuery } from '@mui/material'
+import { useSelector } from 'react-redux'
+
+import Navbar from 'components/Navbar'
+
+const Layout = () => {
+  return (
+    <Box width='100%' height='100%'>
+      <Box>
+        <Navbar />
+        <Outlet />
+      </Box>
+    </Box>
+  )
+}
+
+export default Layout
